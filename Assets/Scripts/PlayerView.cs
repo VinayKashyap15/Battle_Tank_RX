93418f35ab1d1ae4327dd50d879f3eeb0bfe7395
Vinay <vinay@outscal.com>
2019-02-21 19:54:35 +0530

    react working

---
+++ Assets/Scripts/PlayerView.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+public class PlayerView : MonoBehaviour
+{
+    IDisposable _stopCoroutine;
+    // Use this for initialization
+    void Start()
+    {
+        this.UpdateAsObservable()
+            .Subscribe(_=>
+            Debug.Log("Update call hai"));
+        this.OnCollisionEnterAsObservable()
+            .Subscribe(collision =>
+            {
+                Debug.Log(collision.gameObject.name);
+            });
+        var testCoroutine = Observable.FromCoroutine(TestCoroutine1).SelectMany(TestCoroutine2).Subscribe();
+       // _stopCoroutine = Observable.FromCoroutine(TestCoroutine1).SelectMany(TestCoroutine2).Subscribe();
+    }
+
+    private IEnumerator TestCoroutine1()
+    {
+        Debug.Log("inside 1st test");
+        yield return new WaitForSeconds(5);
+        Debug.Log("inside 1st test after 5 seconds");
+    }
+    private IEnumerator TestCoroutine2()
+    {
+        Debug.Log("inside 2nd test");
+        yield return new WaitForSeconds(2);
+        Debug.Log("inside 2nd test after 2 seconds");
+    }
+
+
+}
